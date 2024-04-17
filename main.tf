778a0d44005d1b9c2d81c2c828d1f8976cbee3b2
thiernomadjou <thiernomadjou@gmail.com>
2024-04-17 14:39:20 +0200

    "

--- main.tf
+++ main.tf
@@ -29,3 +29,19 @@ resource "aws_eip" "ip" {
   vpc      = true
   instance = aws_instance.example_a.id
 }
+
+resource "aws_s3_bucket" "example" { }
+
+resource "aws_instance" "example_c" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  depends_on = [aws_s3_bucket.example]
+}
+
+module "example_sqs_queue" {
+  source  = "terraform-aws-modules/sqs/aws"
+  version = "3.3.0"
+
+  depends_on = [aws_s3_bucket.example, aws_instance.example_c]
+}
